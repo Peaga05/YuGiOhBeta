ddfc38d68e963b57bd3608f5529aea42680b36d6
Guilherme <guilhermeoliveira13579112@gmail.com>
2022-11-27 10:49:26 -0300

    criando entity

--- YuGiOh01/YuGiOhEntity.Context.cs
+++ YuGiOh01/YuGiOhEntity.Context.cs
@@ -26,15 +26,15 @@ namespace YuGiOh01
         }
     
         public virtual DbSet<Armadilha> Armadilhas { get; set; }
-        public virtual DbSet<Atributo> Atributoes { get; set; }
+        public virtual DbSet<Atributo> Atributos { get; set; }
         public virtual DbSet<Carta> Cartas { get; set; }
         public virtual DbSet<Icone> Icones { get; set; }
         public virtual DbSet<LogUsuario> LogUsuarios { get; set; }
         public virtual DbSet<Magia> Magias { get; set; }
-        public virtual DbSet<Monstro> Monstroes { get; set; }
-        public virtual DbSet<MonstroPendulo> MonstroPenduloes { get; set; }
+        public virtual DbSet<Monstro> Monstros { get; set; }
+        public virtual DbSet<MonstroPendulo> MonstrosPendulos { get; set; }
         public virtual DbSet<TipoCarta> TipoCartas { get; set; }
-        public virtual DbSet<TipoMonstroEfeito> TipoMonstroEfeitoes { get; set; }
+        public virtual DbSet<TipoMonstroEfeito> TipoMonstrosEfeitos { get; set; }
         public virtual DbSet<Usuario> Usuarios { get; set; }
     }
 }

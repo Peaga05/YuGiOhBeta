3885a1e4390d576879b23d295f66c703e918800f
Guilherme <guilhermeoliveira13579112@gmail.com>
2022-11-27 09:54:38 -0300

    criando o entity

--- YuGiOh01/YuGiOhBDEntity.Designer.cs
+++ YuGiOh01/YuGiOhBDEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Itix\Documents\IFES\CRUD YuGiOh\YuGiOh01\YuGiOhBD.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\guilh\Source\Repos\YuGiOhBeta\YuGiOh01\YuGiOhBDEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

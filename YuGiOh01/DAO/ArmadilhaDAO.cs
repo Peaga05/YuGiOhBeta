c7378d78b4950ef33fc9c720f552103e5b9cee05
Peaga <phserafim2020@gmail.com>
2022-11-28 09:22:24 -0300

    Crud de armailha completo

---
+++ YuGiOh01/DAO/ArmadilhaDAO.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace YuGiOh01.Paginas.Formularios
+{
+    internal class ArmadilhaDAO
+    {
+        internal static void AlterarArmadilha(Armadilha am)
+        {
+			try
+			{
+				using (var ctx = new YuGiOhBDEntities())
+				{
+					var armadilha = ctx.Armadilhas.FirstOrDefault(
+							x => x.IdArmadilha == am.IdArmadilha
+						);
+
+					armadilha.Descricao = am.Descricao;
+					ctx.SaveChanges();
+				}
+			}
+			catch (Exception ex)
+			{
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        internal static void CadastrarArmadilha(Armadilha am)
+        {
+			try
+			{
+				using (var ctx = new YuGiOhBDEntities())
+				{
+					ctx.Armadilhas.Add(am);
+					ctx.SaveChanges();
+				}
+			}
+			catch (Exception ex)
+			{
+
+				throw;
+            }
+        }
+
+        internal static List<Armadilha> ObterArmadilhas()
+        {
+			List<Armadilha> armadilhas = null;
+			try
+			{
+				using(var ctx = new YuGiOhBDEntities())
+				{
+					armadilhas = ctx.Armadilhas.OrderBy(x => x.Descricao).ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+
+                throw new Exception(ex.Message);
+            }
+
+			return armadilhas;
+        }
+
+        internal static Armadilha ObterArmadilha(int id)
+        {
+			Armadilha armadilha = null;
+
+			try
+			{
+                using (var ctx = new YuGiOhBDEntities())
+                {
+					armadilha = ctx.Armadilhas.FirstOrDefault(
+							x => x.IdArmadilha == id
+						);
+                }
+            }
+			catch (Exception ex)
+			{
+
+                throw new Exception(ex.Message);
+            }
+
+			return armadilha;
+        }
+
+        internal static void ExcluirArmadilha(int id)
+        {
+			try
+			{
+				using (var ctx = new YuGiOhBDEntities())
+				{
+					var armadilha = ctx.Armadilhas.FirstOrDefault(
+							x => x.IdArmadilha == id
+						);
+
+					ctx.Armadilhas.Remove(armadilha);
+					ctx.SaveChanges();
+				}
+			}
+            catch (DbUpdateException sqlEx)
+            {
+                throw new DbUpdateException(sqlEx.Message);
+            }
+            catch (Exception ex)
+			{
+
+				throw new Exception(ex.Message);
+			}
+        }
+    }
+}
--- YuGiOh01/Paginas/Home.aspx
+++ YuGiOh01/Paginas/Home.aspx
@@ -23,20 +23,23 @@
 
         <div class="collapse navbar-collapse justify-content-end pe-1" id="myNav">
             <ul class="navbar-nav">
-                
+
+                <li class="nav-item">
+                    <a class="nav-link" href="~/Paginas/Home.aspx">Home</a>
+                </li>
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gerenciar
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" runat="server" href="~/Paginas/Formularios/FrmMonstro.aspx">Gerenciar Monstro</a></li>
                         <li><a class="dropdown-item" href="#">Gerenciar Tipo Carta</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Gerenciar Armadilhas</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="~/Paginas/Home.aspx" >Home</a>
+                    <a class="nav-link" href="">Gerenciar Carta</a>
                 </li>
-
 
                 <li class="nav-item">
                     <a class="nav-link" href="" data-toggle="tab">Sair</a>
